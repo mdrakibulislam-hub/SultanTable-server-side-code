9591e50d2052bd4555965c7d97be5b28edb1f46d
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-05-02 01:15:07 +0600

    Server data created

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const allData = require("./dataset.json");
 
 app.use(cors())
 
 app.get('/', (req, res) => { res.json({ name: "rakib" }) })
+app.get('/alldata', (req, res) => { res.json(allData) })
 
 app.listen(5000, () => { console.log("server..."); })
