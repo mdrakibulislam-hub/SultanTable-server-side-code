59fdb03dccd6740747bcbac865709accd14d811c
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-05-02 22:22:51 +0600

    cached node_modules and added dynamic data loader

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ app.use(cors())
 
 app.get('/', (req, res) => { res.json({ name: "rakib" }) })
 app.get('/alldata', (req, res) => { res.json(allData) })
+app.get('/alldata/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const filteredData = allData.filter(data => data.id === id)
+    res.json(filteredData)
+    console.log(id);
+})
 
 app.listen(5000, () => { console.log("server..."); })
